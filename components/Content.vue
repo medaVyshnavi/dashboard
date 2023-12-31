dec1c860d0d74d56cc9661c62c092543e8d37a92
medaVyshnavi <vyshnavivenkatesh@gmail.com>
2023-12-31 20:36:18 +0900

    Pagination component added

--- components/Content.vue
+++ components/Content.vue
@@ -3,7 +3,7 @@
     <h1 class="text-4xl font-bold">データセット</h1>
     <h4 class="text-xl font-normal opacity-40 py-2">データセットを管理する</h4>
     <Tab />
-    <div class="mt-8 flex justify-between items-center">
+    <div class="mt-6 flex justify-between">
       <div>
         <Button />
         <Button />
@@ -11,6 +11,7 @@
       <Search />
     </div>
     <Table />
+    <Pagination />
   </div>
 </template>
 
@@ -19,4 +20,5 @@ import Tab from "./Tab.vue";
 import Table from "./Table.vue";
 import Button from "./Buttons.vue";
 import Search from "./Search.vue";
+import Pagination from "./Pagination.vue";
 </script>
---
+++ components/Pagination.vue
@@ -0,0 +1,62 @@
+<template>
+  <nav aria-label="Page navigation example" class="mt-8 flex justify-end">
+    <ul class="flex items-center -space-x-px h-10 text-base">
+      <li>
+        <a
+          href="#"
+          class="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700"
+        >
+          <span class="sr-only">Previous</span>
+          <IconsLeftArrow />
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+          >1</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+          >2</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          aria-current="page"
+          class="z-10 flex items-center justify-center px-4 h-10 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700"
+          >3</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+          >4</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+          >5</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700"
+        >
+          <span class="sr-only">Next</span>
+          <IconsRightArrow />
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script setup lang="ts"></script>
